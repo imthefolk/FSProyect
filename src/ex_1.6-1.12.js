f87499014241155ecf4d001985df27062750c9c7
dvfolkmanas <vfolkmanas@grupobme.es>
2023-02-08 17:42:49 +0100

    Exercise 1.7 (unfinished)

--- src/ex_1.6-1.12.js
+++ src/ex_1.6-1.12.js
@@ -6,11 +6,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0) 
 
   return (
     <div>
       <h1> give feedback  </h1>
-        <button onClick={()=> setGood(good+1)}>good</button> 
+        <button onClick={()=> setGood(good+1), setAverage(average+1)}>good</button> 
         <button onClick={()=> setNeutral(neutral+1)}>neutral</button>
         <button onClick={()=> setBad(bad+1)}>bad</button> 
         
@@ -18,6 +19,9 @@ const App = () => {
         <p>good {good}</p> 
         <p>neutral {neutral}</p> 
         <p>bad {bad}</p>  
+        <p>all sum={bad+neutral+good}</p> 
+        <p>bad {bad}</p>  
+
     </div>  
   )
 }
