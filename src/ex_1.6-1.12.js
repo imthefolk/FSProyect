c33d0bb2fda35092c9232873d29bd308aaf50dd0
dvfolkmanas <vfolkmanas@grupobme.es>
2023-02-14 17:00:58 +0100

    Exercises 1.8 and 1.9

--- src/ex_1.6-1.12.js
+++ src/ex_1.6-1.12.js
@@ -1,29 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({good, neutral, bad, average}) => {
+  const sum=bad+neutral+good
+  if (sum === 0)
+  { 
+    return (
+    
+      <>
+        <h1> statistics  </h1>
+        <p> No feedback given</p>
+      </>) 
+  } 
+
+
+  return (
+    <>
+      <h1> statistics  </h1>
+      <p>good {good}</p> 
+      <p>neutral {neutral}</p> 
+      <p>bad {bad}</p>  
+      <p>all {sum}</p> 
+      <p>average {average/sum} </p>  
+      <p>positive {(good*100)/sum} %</p>
+    </>
+  )      
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [average, setAverage] = useState(0) 
-  const sum=bad+neutral+good
+
   return (
     <div>
       <h1> give feedback  </h1>
         <button onClick={()=>{setGood(good+1); setAverage(average+1)}}>good</button> 
         <button onClick={()=> setNeutral(neutral+1)}>neutral</button>
         <button onClick={()=>{setBad(bad+1); setAverage(average-1)}}>bad</button> 
-        
-      <h1> statistics  </h1>
-        <p>good {good}</p> 
-        <p>neutral {neutral}</p> 
-        <p>bad {bad}</p>  
-        <p>all {sum}</p> 
-        <p>bad {bad}</p>
-        <p>average {average/sum} </p>  
-        <p>positive {(good*100)/sum} %</p>
 
+        <Statistics good={good} neutral={neutral} bad={bad} average={average} /> 
     </div>  
   )
 }
