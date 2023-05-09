1afd309ed1ed97ddb5fd1691fdaa92672f81ddd9
imthefolk <victorfolk16@hotmail.com>
2023-05-09 13:22:22 +0200

    Exercise 3.9: Deploy to Heroku. Port added and Procfile added

--- index.js
+++ index.js
@@ -135,6 +135,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = proccess.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
