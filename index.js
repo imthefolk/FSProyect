02b65925a5fcaffc6fdeff7b943a4d06c384e4b5
imthefolk <victorfolk16@hotmail.com>
2023-05-10 10:10:15 +0200

    Exercise 3.9: port test

--- index.js
+++ index.js
@@ -136,5 +136,6 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => { 
+  console.log(`Server running on port ${PORT}`)
+})
